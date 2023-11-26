90348d505ce8553f9f74de0b233c0ec13092d71b
Pandhu Arya <pandhu.munjalindra@gmail.com>
2023-11-27 04:55:45 +0700

    [fix] fixed url path

--- routes/route.js
+++ routes/route.js
@@ -4,16 +4,16 @@ const path = require('path');
 const authController = require('../controller/controller'); // Pastikan Anda menyesuaikan path ini.
 
 router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + '/index.html'));
+    res.sendFile(path.join(__dirname + '../index.html'));
 });
 
 router.get('/register', (req, res) => {
-    res.sendFile(path.join(__dirname + '/register.html'));
+    res.sendFile(path.join(__dirname + '../register.html'));
 });
 
 router.get('/home', (req, res) => {
     if (req.session.loggedin) {
-        res.sendFile(path.join(__dirname + '/home.html'));
+        res.sendFile(path.join(__dirname + '../home.html'));
     } else {
         const errorMessage = 'You most login to see this page';
         res.send(`<script>alert('${errorMessage}'); window.location.href='/';</script>`);
